5b3fa6d45c1a33b06636f35ca924fc7b89757476
sandriverfish <mc6066@outlook.com>
2024-12-05 16:09:54 +0800

    merge v0.1

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -20,7 +20,7 @@ function HomepageHeader() {
           <Link
             className="button button--secondary button--lg"
             to="/docs/intro">
-            Docusaurus Tutorial - 5min ⏱️
+            Dexter Robotics Arm ROS Course - 120min ⏱️
           </Link>
         </div>
       </div>
